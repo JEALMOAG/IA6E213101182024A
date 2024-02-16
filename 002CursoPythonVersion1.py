adf0d801a350595b005e579f2641070f4f7e11c3
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-16 14:11:19 -0600

    Create 002CursoPythonVersion1.py
    Se ha creado el algoritmo que contiene las practicas 5 y 6, del curso de Python

---
+++ 002CursoPythonVersion1.py
@@ -0,0 +1,9 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+16 febrero 2024
+Practica 5 y 6
+"""
+msj1 = "Welcome to the party, this is your 'home'. "
+msj2 = 'My name is "Jesus Montes."'
+result = msj1 + msj2
+print(result) 
