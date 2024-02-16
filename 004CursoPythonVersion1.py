846b129c3d2f96b4b64fea294d55153531815b38
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-16 16:11:45 -0600

    Create 004CursoPythonVersion1.py
    Archivo 4, que contiene las practicas de l a 12 a 14, (upper( ), lower( ) and title( ))

---
+++ 004CursoPythonVersion1.py
@@ -0,0 +1,12 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+16 febrero 2024
+Practica 12, 13 y 14
+"""
+name =  "enrique barros fernandez".title()
+print(name)
+frase1 = "Esta Es Una Frase Para Ser Formateada".lower()
+print(frase1)
+frase2 = "Esta Es Una Frase Para Ser Formateada".upper()
+print(frase2)
+
