ec40e5d6287901d527da7a28e1eb41a2741075d2
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-16 15:43:10 -0600

    Create 003CursoPythonVersion1.py
    Documento 3, el cual tiene las practicas de la 7 a la 11.

---
+++ 003CursoPythonVersion1.py
@@ -0,0 +1,19 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+16 febrero 2024
+Practica 7, 8, 9, 10 y 11
+"""
+cadena = "hello I'm jesus montes" + "and I'm a student"
+print(cadena)
+sms1 = "Hi I'm jesus montes"
+sms2 = "and I'm a student"
+rst = sms1 + sms2
+print(sms1 + " " + sms2)
+print(sms1 +  " " + "and I like the electronic")
+name1 = "Jesus"
+name2 = "Alejandro"
+lName1 = "Montes" #last Name one
+lName2 = "Aguila"
+fName =  name1 + " " + name2 + " " + lName1 + " " + lName2
+print(fName)
+print("34"+"43")
