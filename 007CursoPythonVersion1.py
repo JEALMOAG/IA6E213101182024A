e898bbc8671fb9586909cc3d825ba141dce10cef
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-19 02:49:26 -0600

    Create 007CursoPythonVersion1.py
    exponenciacion mediante el metodo de doble operador multiplicador (**) y el metodo de multiplicar el mismo numero hasta alcanzar el resultado deseado

---
+++ 007CursoPythonVersion1.py
@@ -0,0 +1,7 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+18 febrero 2024
+Practica 21"""
+'''Utilizando solo el número 2 (cuantas veces necesites) y sin utilizar exponentes, debes obtener el resultado 1024.'''
+wishNumber = 2*2*2*2*2*2*2*2*2*2
+print(wishNumber)
