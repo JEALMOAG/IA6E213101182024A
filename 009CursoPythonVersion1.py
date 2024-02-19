50d7e7f838df179a70f1f9463d7d17eb04dcc1e9
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-19 02:53:16 -0600

    Create 009CursoPythonVersion1.py
    Creacion de listas practicas 23-25

---
+++ 009CursoPythonVersion1.py
@@ -0,0 +1,10 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+18 febrero 2024
+Practica 23,24 y 25"""
+array = ["Rojo","Azul","Verde","Amarillo","Marron","Lila","Negro","Rosa"]
+print("El color que esta en la posicion 3 es",array[3])
+print("El color rojo esta en : 0")
+print("El color rosa esta en: 7")
+array2 = ["tres","dos","cinco","cuatro","uno"]
+print(array2)
