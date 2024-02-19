72b0cf647caf156275f430ce6901469ae4eedf17
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-19 02:54:07 -0600

    Create 010CursoPythonVersion1.py
    posiciones negativas en listas, practica 26

---
+++ 010CursoPythonVersion1.py
@@ -0,0 +1,10 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+18 febrero 2024
+Practica 26"""
+colores = ['rojo','azul','verde','amarillo','marron','lila','negro','rosa','blanco','naranja']
+print(colores[-1])
+print(colores[-7])
+print(colores[-5])
+print(colores[-2])
+print(colores[-10])
