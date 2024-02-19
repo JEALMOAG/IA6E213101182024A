1d9be14c0ad64282ea3265f54300d87a314250a0
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-19 02:55:06 -0600

    Create 011CursoPythonVersion1.py
    borrado por posicion, metodo "del"
    del lista[posicion]

---
+++ 011CursoPythonVersion1.py
@@ -0,0 +1,10 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+18 febrero 2024
+Practica 27"""
+colores = ['rojo','azul','verde','amarillo','marron','lila','negro','rosa','blanco','naranja']
+del colores[1]
+del colores[-6]
+del colores[-4]
+del colores[-3]
+print(colores)
