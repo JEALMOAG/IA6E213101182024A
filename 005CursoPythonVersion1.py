9a506d86ef757d8d9c1f55e5e70b9ff13148b8da
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-02-19 02:45:59 -0600

    Create 005CursoPythonVersion1.py
    Concatenacion y salto de linea de cadenas (Strings)

---
+++ 005CursoPythonVersion1.py
@@ -0,0 +1,7 @@
+"""
+Jesus Alejandro Montes Aguila 21310118
+16 febrero 2024
+Practica 15
+"""
+languages = "Programation Languages:\n\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.\n\t-C.\n\t-C++." 
+print(languages)
