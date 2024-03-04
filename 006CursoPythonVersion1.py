8565df3f01d2ca2c032d57e77b1ba67c45994c41
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-03-03 23:35:18 -0600

    Update 006CursoPythonVersion1.py
    se hace una descripcion del programa mediante comentarios

--- 006CursoPythonVersion1.py
+++ 006CursoPythonVersion1.py
@@ -2,9 +2,12 @@
 Jesus Alejandro Montes Aguila 21310118
 18 febrero 2024
 Practica 16,17,18,19 y 20""""
+#calculadora, se hace la definicon de 4 variables suma, resta, multiplicacion y division las \\
+#las cuales cada una opera con diferentes valores.
 suma = 20 + 23 + 40 + 4
 resta = 20 + 23 - 130
 multi = 20*23*((1)/(23*20))*870
 div = (5000/230)/2.1
 operacion = 1*(10/5+(15-17))
 print("\n",suma,"\n",resta,"\n",multi,"\n",div,"\n",operacion)
+#se imprime el resultado de cada operacion dando salto de linea con \n 
