a75f87bcc0db34cd1530b6bcb6c7e08fdeb7a28c
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-03-03 23:33:46 -0600

    Update 004CursoPythonVersion1.py
    Se agregaron comentarios clave en ciertas instrucciones del codigo

--- 004CursoPythonVersion1.py
+++ 004CursoPythonVersion1.py
@@ -3,10 +3,10 @@ Jesus Alejandro Montes Aguila 21310118
 16 febrero 2024
 Practica 12, 13 y 14
 """
-name =  "enrique barros fernandez".title()
+name =  "enrique barros fernandez".title()#cada una de las palabras se iniciaran en mayusculas
 print(name)
-frase1 = "Esta Es Una Frase Para Ser Formateada".lower()
+frase1 = "Esta Es Una Frase Para Ser Formateada".lower()#toda las frase se convertira en minusculas
 print(frase1)
-frase2 = "Esta Es Una Frase Para Ser Formateada".upper()
+frase2 = "Esta Es Una Frase Para Ser Formateada".upper()#toda la frase se converira en mayusculas
 print(frase2)
 
