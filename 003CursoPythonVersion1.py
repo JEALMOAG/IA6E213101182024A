ede257ffb88440e40646d47935836a5862915a04
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-03-03 23:22:37 -0600

    Update 003CursoPythonVersion1.py
    Se le han agregado comentarios en ciertas instrucciones de los algoritmos.

--- 003CursoPythonVersion1.py
+++ 003CursoPythonVersion1.py
@@ -3,17 +3,18 @@ Jesus Alejandro Montes Aguila 21310118
 16 febrero 2024
 Practica 7, 8, 9, 10 y 11
 """
-cadena = "hello I'm jesus montes" + "and I'm a student"
-print(cadena)
+#concatenacion de variables y dentro las variables 
+cadena = "hello I'm jesus montes" + "and I'm a student" # se define una varible string con concatenacion interna 
+print(cadena) #se imprime la varible con concatenacion 
 sms1 = "Hi I'm jesus montes"
 sms2 = "and I'm a student"
-rst = sms1 + sms2
-print(sms1 + " " + sms2)
+rst = sms1 + sms2 # se guarda en unvaribale la concatenacion del sms1 y sms2
+print(sms1 + " " + sms2)# se concatena con un caracter " " de separacion.
 print(sms1 +  " " + "and I like the electronic")
 name1 = "Jesus"
 name2 = "Alejandro"
 lName1 = "Montes" #last Name one
 lName2 = "Aguila"
-fName =  name1 + " " + name2 + " " + lName1 + " " + lName2
-print(fName)
-print("34"+"43")
+fName =  name1 + " " + name2 + " " + lName1 + " " + lName2 # fName almacena mi nombre al concatenar name1,nmae2,lName1,lName2.
+print(fName)# se imprime mi nombre
+print("34"+"43") # se hace la concatenacion de strings numericos en esta caso sera 3443 y no la suma de 34 + 43
