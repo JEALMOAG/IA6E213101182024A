97b8ca4d3b8095a7d846134e9c940f8f3ce23d98
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-03-03 23:11:06 -0600

    Update 001CursoPythonVersion1.py
    Se le an agragado comentarios a las isntrucciones del codigo

--- 001CursoPythonVersion1.py
+++ 001CursoPythonVersion1.py
@@ -3,8 +3,8 @@ Jesus Alejandro Montes Aguila 21310118
 16 febrero 2024
 Practica 1, 2, 3 y 4
 """
-msj = "Hola soy jesus montes"
-a = 1
+msj = "Hola soy jesus montes" # se define un variable tipo string
+a = 1 #variable tipo entero
 b = 4
-c = a + b
-print(msj,"\n",c)
+c = a + b #suma de varibles tipo entero
+print(msj,"\n",c) # se imprime en pantalla la variable msj y la suma.
