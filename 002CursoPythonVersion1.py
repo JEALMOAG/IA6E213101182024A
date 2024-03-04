c6f868be67ce5b9f3b6ab099bef7fb338adb6a00
JEALMOAG <151214106+JEALMOAG@users.noreply.github.com>
2024-03-03 23:21:58 -0600

    Update 002CursoPythonVersion1.py
    Se le han agregado comentarios adicionales a las instrucciones del algoritmo.

--- 002CursoPythonVersion1.py
+++ 002CursoPythonVersion1.py
@@ -3,7 +3,7 @@ Jesus Alejandro Montes Aguila 21310118
 16 febrero 2024
 Practica 5 y 6
 """
-msj1 = "Welcome to the party, this is your 'home'. "
-msj2 = 'My name is "Jesus Montes."'
-result = msj1 + msj2
-print(result) 
+msj1 = "Welcome to the party, this is your 'home'. " # se define una variable tipo sring
+msj2 = 'My name is "Jesus Montes."' # se definie una segunda variable tipo string
+result = msj1 + msj2 # se hace la concatenacion de las 2 varibales 
+print(result) # se imprime la concatenacion
